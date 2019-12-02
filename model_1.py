8f03b01dc5adb806f5a73534d793861aaf0606c9
Akshay Singh Rana <spawnakshay@gmail.com>
2019-12-02 13:39:00 -0500

    initial setup

---
+++ model_1.py
@@ -0,0 +1,28 @@
+
+import preprocessing
+import utils
+
+class Model1:
+    '''
+    This class is used to code the first approach in the presentation
+    of using each data source for every challenge.
+    '''
+
+    def __init__(self):
+        pass
+
+    def fetch_face_data(self):
+        df_face = ""
+        return df_face
+    
+    def fetch_text_data(self):
+        df_text = ""
+        return df_text
+    
+    def fetch_relation_data(self):
+        df_relation = ""
+        return df_relation
+    
+    def fetch_node2vec_data(self):
+        df_n2v = ""
+        return df_n2v
